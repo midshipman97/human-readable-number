d21adcb280e2f3a0ce9b6b2040f9817c5fd267d5
midshipman97 <misganztmk@gmail.com>
2022-07-01 14:00:53 +0700

    feat: started coding in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+  this.words = {
+    l0: ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','nineteen'],
+    l1: ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'],
+    l2: ['hundred']
+  };
+  let outNumber = '';
+  let levelOfNumber = Math.floor(number / 10)
 }
